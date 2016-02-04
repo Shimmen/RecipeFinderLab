3cfa01a3b4e2fb8f25316379bea9cb48dee0e237
Simon Moos <simon.moos95@gmail.com>
2016-02-04 17:03:41 +0100

    Begin work on recipe detail view

--- src/recipefinder/ResultViewController.java
+++ src/recipefinder/ResultViewController.java
@@ -5,7 +5,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import se.chalmers.ait.dat215.lab2.Ingredient;
 import se.chalmers.ait.dat215.lab2.Recipe;
 
 import java.io.IOException;
@@ -16,19 +21,46 @@ import java.util.stream.Collectors;
 
 public class ResultViewController implements Initializable {
 
+    private Parent searchView;
     private List<Recipe> recipes;
 
-    @FXML Button backButton;
-    @FXML ListView<Recipe> resultList;
+    @FXML private Button backButton;
+    @FXML private ListView<Recipe> resultList;
 
-    private Parent searchView;
+    @FXML private ScrollPane recipeScrollView;
+    @FXML private AnchorPane scrollContentView;
+
+    @FXML private Label recipeName;
+    @FXML private Text description;
+    @FXML private Text ingredients;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        scrollContentView.prefWidthProperty().bind(recipeScrollView.widthProperty());
+        description.wrappingWidthProperty().bind(recipeScrollView.widthProperty().subtract(14.0 * 2 + 10.0));
+
         resultList.getSelectionModel().selectedItemProperty().addListener((observable, prevSelectedRecipe, selectedRecipe) -> {
+
+            if (selectedRecipe == null) {
+                return;
+            }
+
             // Set recipe detail view properties
-            // TODO!
+
+            recipeName.setText(selectedRecipe.getName());
+            description.setText(selectedRecipe.getDescription());
+
+            String ingredientsString = selectedRecipe.getIngredients().stream()
+                    .map(this::ingredientString)
+                    .reduce("", (s, s2) -> s + "\n" + s2);
+            ingredients.setText(ingredientsString);
+
         });
+    }
+
+    private String ingredientString(Ingredient ingredient) {
+        return ingredient.getAmount() + " " + ingredient.getUnit() + " " + ingredient.getName();
     }
 
     public void setRecipes(List<Recipe> recipes, boolean includeAll) {
